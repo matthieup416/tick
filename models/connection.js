bda252cd16c6151a22b52a5c5e8d712a1c2d6063
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2022-04-17 18:07:36 +0200

    m

--- models/connection.js
+++ models/connection.js
@@ -1,28 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 // useNewUrlParser ;)
 var options = {
   connectTimeoutMS: 5000,
   useNewUrlParser: true,
-  useUnifiedTopology: true
- };
+  useUnifiedTopology: true,
+}
 
- var identifiants = {
-   mdp: 'password',
-   identifiant: 'identifiant',
-   nomCluster: 'CLUSTER'
- }
-
- 
+var identifiants = {
+  mdp: 'test',
+  identifiant: 'test',
+  nomCluster: 'Ticketac',
+}
 
 // --------------------- BDD -----------------------------------------------------
-mongoose.connect(`mongodb+srv://${identifiants.identifiant}:${identifiants.mdp}@cluster0.orqbr.mongodb.net/Ticketac?retryWrites=true`,
-   options,
-   function(err) {
+mongoose.connect(
+  `mongodb+srv://${identifiants.identifiant}:${identifiants.mdp}@cluster0.orqbr.mongodb.net/Ticketac?retryWrites=true`,
+  options,
+  function (err) {
     if (err) {
-      console.log(`error, failed to connect to the database because --> ${err}`);
+      console.log(`error, failed to connect to the database because --> ${err}`)
     } else {
-      console.info('*** Database Ticketac connection : Success ***');
+      console.info('*** Database Ticketac connection : Success ***')
     }
-   }
-);
+  }
+)
